8a1bd662a5cac919b780202d6b9b4598da49d0cd
angelDariaux <angeldariaux@gmail.com>
2022-08-31 21:35:02 -0600

    for loops in python

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+
+print('My name is: ')
+for i in range(5):
+  print('Angel Five times ' + str(i))
+
+
+# sum of all numbers from 0 to 100
+total = 0 
+for num in range(100 + 1):
+  total += num
+print(total)
