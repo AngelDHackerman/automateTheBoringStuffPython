4033bdc416712b4793643e3e8ac0a9f0e343dbba
angelDariaux <angeldariaux@gmail.com>
2022-09-03 17:16:23 -0600

    adding better identation to the output and the parttern +

--- pythonScripts/regexPatterns.py
+++ pythonScripts/regexPatterns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import re
 
-      # ! Matching 0 or 1 times
+      # ! Matching 0 or 1 times ?
 
+print('\n\t Match 0 or 1 times:')
 batRegex = re.compile(r'Bat(wo)?man')  # ? the ? states that pattern appears 1 or 0 times
 
 matchObject = batRegex.search('The aventures of Batman')
@@ -12,7 +13,7 @@ matchObject = batRegex.search('The aventures of Batwoman')
 print(matchObject.group())  # in here it appeares 1 time
 
 matchObject = batRegex.search('The aventures of Batwowowowoman')
-print(matchObject)  # None == no matches at all.
+print(matchObject)  # None == because it matches more than 1 time.
 
 
 #todo: This one will match if has or not an area code. 
@@ -26,4 +27,23 @@ print(matchObject.group())
 
 
 
-      # ! Matching 0 or more times
+print('\n\t Matches 0 or more times:')
+
+      # ! Matching 0 or more times *
+
+batRegex = re.compile(r'Bat(wo)*man')
+matchObject = batRegex.search('The aventures of Batman')
+print(matchObject.group())  # Matches 0 times
+
+matchObject = batRegex.search('The aventures of Batwoman')
+print(matchObject.group())  # matches 1 time
+
+matchObject = batRegex.search('The aventures of Batwowowowoman')
+print(matchObject.group())  # matches several times.
+
+
+
+print('\n\t Matches 1 or more times')
+
+      # ! Matching 1 or more times +
+
