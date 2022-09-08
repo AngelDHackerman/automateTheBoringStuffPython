95e4cb6328e2f73a7dfc4d48b4e7ec24df01186c
angelDariaux <angeldariaux@gmail.com>
2022-09-07 20:51:29 -0600

    writting and appending text to a file

--- scriptsPython/writeFile.py
+++ scriptsPython/writeFile.py
@@ -1,2 +1,25 @@
 #!/bin/python3
 
+import os
+
+helloFile = open('./text2.txt', 'w')  # ? 'w' for write and 'a' for append text to the file
+
+helloFile.write('Helloooooooooooo \n')
+helloFile.write('Helloooooooooooo \n')
+helloFile.write('Helloooooooooooo \n')
+helloFile.write('Helloooooooooooo \n')
+
+helloFile.close()
+
+
+
+baconFile = open('./bacon.txt', 'w')  # ? write a new document
+baconFile.write('Bacon is not a vegetable.')
+
+baconFile.close()
+
+baconFile = open('./bacon.txt', 'a')  # ? append text to the document
+baconFile.write('\n\nalso it is delicious.')
+
+
+
