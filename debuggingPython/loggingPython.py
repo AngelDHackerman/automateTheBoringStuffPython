673c7ec8deb080095c2bb9d9657f548d1de00ad8
angelDariaux <angeldariaux@gmail.com>
2022-09-11 08:01:26 -0600

    debugging with python modules

---
+++ debuggingPython/loggingPython.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')  # ? this is the configuration for the debuging.
+# logging.disable(logging.CRITICAL)  # ? this program disable all the logging messages, insted of comment them.
+
+
+logging.debug('Start of program')
+
+def factorial (n):
+  logging.debug('Start of factorial (%s)' % (n))  # %s stands for: substitution, and then we have the % (n) which stands for the variables than will be exchanged.
+  total = 1
+  for i in range (1, n + 1):
+    total *= i
+    logging.debug('i is %s, total is %s' % (i, total))
+
+  logging.debug('Return value is %s ' % (total))
+  return total
+
+print(factorial(5))
+
+logging.debug('End of program')
